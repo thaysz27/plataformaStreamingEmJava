df38c7e070e0401ee4f9ae44c5c7d384af63cdf6
thays <tvcc@cin.ufpe.br>
2025-02-16 23:56:54 -0300

    Add Array

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
 import streaming.modelos.Filme;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Filme filme1 = new Filme();
+        var filme1 = new Filme();
         filme1.setNome("Interestelar");
         filme1.setAnoLancamento(2014);
         filme1.setDuracaoEmMinutos(169);
+        filme1.avalia(10);
 
-        System.out.println(filme1.getTotalDeAvaliacoes());
+
+        var filme2 = new Filme();
+        filme2.setNome("A chegada");
+        filme2.setAnoLancamento(2016);
+        filme2.setDuracaoEmMinutos(116);
+        filme2.avalia(9);
+
+        var filme3 = new Filme();
+        filme3.setNome("Passageiros");
+        filme3.setAnoLancamento(2016);
+        filme3.setDuracaoEmMinutos(116);
+        filme3.avalia(8.6);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filme1);
+        listaDeFilmes.add(filme2);
+        listaDeFilmes.add(filme3);
+        System.out.println(listaDeFilmes);
 
 
     }
